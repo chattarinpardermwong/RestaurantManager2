e5e3693a9a5c4331cf3479a89096825f971ea14e
Legoeiei <chattarin.par1@ku.th>
2018-11-26 23:19:22 +0700

    Initial Commit

---
+++ RestaurantManager2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantManager2
+//
+//  Created by Chattarin.par1 on 26/11/2561 BE.
+//  Copyright © 2561 ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
